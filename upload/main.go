4d8364d2253848f1368d097add29d88b41d6f84d
Philip Jovanovic <philip119@gmx.de>
2025-03-30 03:56:59 +0200

    add single variant option

--- upload/main.go
+++ upload/main.go
@@ -27,3 +27,21 @@ func (n *Client) WebP(image *os.File) (*WebPUpload, *client.Error) {
 
 	return res.Data, nil
 }
+
+func (n *Client) WebPVariant(image *os.File, variant Variant) (*WebPUpload, *client.Error) {
+	var res client.Res[WebPUpload]
+
+	err := n.B.CallMultipart("POST", "/upload/webp/"+variant, image.Name(), image, &res)
+	if err != nil {
+		return nil, err
+	}
+
+	if res.Error != "" {
+		return nil, &client.Error{
+			Code:    res.Error,
+			Message: res.Message,
+		}
+	}
+
+	return res.Data, nil
+}
--- upload/structs.go
+++ upload/structs.go
@@ -6,3 +6,16 @@ type WebPUpload struct {
 	Variants         []string `json:"variants"`
 	FullPaths        []string `json:"fullPaths"`
 }
+
+type Variant = string
+
+const (
+	// full quality, low compression
+	FullQuality Variant = "x1"
+
+	// half quality, medium compression
+	HalfQuality Variant = "x2"
+
+	// third quality, high compression
+	ThirdQuality Variant = "x3"
+)
